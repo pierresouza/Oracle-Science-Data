2bce30322cf17b644462d9d4b393369c6a876d61
pierresouza <pierre.s3@hotmail.com>
2025-04-05 17:55:54 -0300

    update

---
+++ exercicios/exer1.py
@@ -0,0 +1,9 @@
+valor1 = input("Digite o primeiro valor: ")
+valor2 = input("Digite o segundo valor: ")
+
+if valor1 > valor2:
+  print(f'O primeiro valor é o maior!')
+elif valor2 > valor1:
+  print(f'O segundo valor é o maior!')
+else:
+  print(f'Os dois valores são iguais!')
